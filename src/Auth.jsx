0aa7870918d0c13c326540641891555964c9da02
vivianlu <zj50620@yahoo.com.tw>
2023-09-10 16:14:42 +0800

    first commit

---
+++ src/Auth.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Auth = () => {
+  return (
+    <>
+      <div className="bg-yellow">
+        <div className="container signUpPage vhContainer">
+          <div className="side">
+            <img className="logoImg" src="/react_api_todolist/logo_lg.png" alt="logo" />
+            <img className="d-m-n" src="/react_api_todolist/img.png" alt="workImg" />
+          </div>
+          <div>
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Auth;
